aff0411953c93c49a54169847f73989011a78882
Guillaume Dumas <deep@introspection.eu>
2022-07-07 15:13:33 -0400

    PEP8 for basicHKB.py

--- basicHKB.py
+++ basicHKB.py
@@ -17,35 +17,36 @@ import numpy as np
 from torchdiffeq import odeint
 
 
-#Generate parameters to plot phase space
-npdOmega = 0. #Hertz, difference between intrinsic frequencies; can modify the "strength" of an attractor/repeller
-np_a = 5. #strength of in-phase coupling
-np_b = 1. #strength of anti-phase coupling
-#higher values of b with respect to a lead to a larger stable region of anti-phase coupling (try setting a to 5 and varying b to 0,1,2,5)
+# Generate parameters to plot phase space
+npdOmega = 0.  # Hertz, difference between intrinsic frequencies; can modify the "strength" of an attractor/repeller
+np_a = 5.  # strength of in-phase coupling
+np_b = 1.  # strength of anti-phase coupling
+# higher values of b with respect to a lead to a larger stable region of anti-phase coupling (try setting a to 5 and varying b to 0,1,2,5)
 
 
-#Generate phase range
-phiRange = np.linspace(-np.pi, 2*np.pi, 540)
+# Generate phase range
+phiRange = np.linspace(-np.pi, 2 * np.pi, 540)
+
 
 def npHKBextended(phi):
     "Extended HKB equation."
-    return npdOmega-np_a*np.sin(phi)-2*np_b*np.sin(2*phi)
+    return npdOmega - np_a * np.sin(phi) - 2 * np_b * np.sin(2 * phi)
 
 
 # Visualization of the phase space and find fixed points
-for i in range(1,len(phiRange-1)):
-    #indicate attractors in green
+for i in range(1, len(phiRange - 1)):
+    # indicate attractors in green
     print(npHKBextended(phiRange[i]))
-    if (npHKBextended(phiRange[i]) < 0) & (npHKBextended(phiRange[i-1]) > 0):
-       plt.scatter(phiRange[i],npHKBextended(phiRange[i]), color = 'green')
-    #indicate repellers in red
-    elif (npHKBextended(phiRange[i]) > 0) & (npHKBextended(phiRange[i-1]) < 0):
-       plt.scatter(phiRange[i],npHKBextended(phiRange[i]), color = 'red')
+    if (npHKBextended(phiRange[i]) < 0) & (npHKBextended(phiRange[i - 1]) > 0):
+        plt.scatter(phiRange[i], npHKBextended(phiRange[i]), color='green')
+    # indicate repellers in red
+    elif (npHKBextended(phiRange[i]) > 0) & (npHKBextended(phiRange[i - 1]) < 0):
+        plt.scatter(phiRange[i], npHKBextended(phiRange[i]), color='red')
 plt.plot(phiRange, npHKBextended(phiRange))
 plt.show()
 
 
-########Simulation#########
+# #######Simulation#########
 
 # Increase precision of float numbers
 torch.set_default_dtype(torch.float64)
@@ -57,10 +58,10 @@ torch.pi = torch.acos(torch.zeros(1)).item() * 2
 duration = torch.as_tensor([2.])  # seconds
 fs = torch.as_tensor([500])  # Hertz
 fosc = torch.as_tensor([10.])  # Hertz
-#phi0 = torch.as_tensor([1.])  # Radians
-phi0 = torch.as_tensor([torch.pi+0.1])  # Radians
-a = torch.as_tensor([np_a]) #phase coupling
-b = torch.as_tensor([np_b]) #anti-phase coupling
+# phi0 = torch.as_tensor([1.])  # Radians
+phi0 = torch.as_tensor([torch.pi + 0.1])  # Radians
+a = torch.as_tensor([np_a])  # phase coupling
+b = torch.as_tensor([np_b])  # anti-phase coupling
 dOmega = torch.as_tensor([0.])
 
 # Generate time
@@ -69,21 +70,20 @@ t = torch.linspace(start=0, end=float(duration), steps=int(duration * fs))
 
 def HKB(t, phi):
     "Simple HKB equation."
-    return torch.as_tensor([-a*torch.sin(phi)-2*b*torch.sin(2*phi)])
+    return torch.as_tensor([- a * torch.sin(phi) - 2 * b * torch.sin(2 * phi)])
 
 
 def HKBextended(t, phi):
     "Extended HKB equation."
-    return torch.as_tensor([dOmega-a*torch.sin(phi)-2*b*torch.sin(2*phi)])
+    return torch.as_tensor([dOmega - a * torch.sin(phi) - 2 * b * torch.sin(2 * phi)])
+
 
 # Solve the ODE for different initial values of phi0
-for i in np.linspace(0,2*np.pi, 20):
+for i in np.linspace(0, 2 * np.pi, 20):
     phi0 = torch.as_tensor([i])
     phit = odeint(HKBextended, phi0, t)
-    plt.plot(t, phit, color = 'blue')
+    plt.plot(t, phit, color='blue')
 
 # Visualization of the phase trajectory
-plt.ylim([0,2*np.pi])
+plt.ylim([0, 2 * np.pi])
 plt.show()
-
-
