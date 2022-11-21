203794cd09db89618c2447bb3bcc33536099edd5
NicolasCoucke <58745129+NicolasCoucke@users.noreply.github.com>
2022-11-21 15:14:43 +0100

    Update social_agent_evaluation.py

--- social_agent_evaluation.py
+++ social_agent_evaluation.py
@@ -2,7 +2,7 @@ import numpy as np
 from utils import symmetric_matrix, eucl_distance, initiate_coupling_weights
 from environment import Environment, Social_environment, Social_stimulus_environment
 from simulations import evaluate_parameters_social
-from visualizations import plot_multi_agent_run
+from visualizations import plot_multi_agent_run, multi_agent_animation
 from agent_RL import Gina, Guido, MultipleGuidos
 
 import time
@@ -26,7 +26,7 @@ def calculate_KOP(phase_matrix):
    KOP_std = np.std(KOP_in_time)
    return KOP_in_time, KOP_std
 
-def calculate_average_PLV(phase_matrix, window_length, window_step):
+def calculate_average_PLV_individual(phase_matrix, window_length, window_step):
    # calculate windowed PLV
    window_start = 0
    window_end = window_start + window_length
@@ -59,7 +59,7 @@ def calculate_average_PLV(phase_matrix, window_length, window_step):
 
 
 # define variables for environment
-fs = 50 # Hertz
+fs = 30 # Hertz
 duration = 30 # Seconds
 stimulus_positions = [np.array([-100, 0]), np.array([100,0])] # m, m
 stimulus_decay_rate = 0.02 # in the environment
@@ -71,7 +71,7 @@ stimulus_ratio = 0.8
 agent_radius = 2.5
 agent_eye_angle = 0.5 * np.pi # 45 degrees
 starting_position = [0, -100] 
-starting_orientation = -0.25*np.pi
+starting_orientation = 0.2
 starting_distances = [100]#np.linspace(95, 105, )
 starting_orientations = [0.2] # np.linspace(-np.pi/2, np.pi/2, 5)
 environment = "double_stimulus"
@@ -81,21 +81,24 @@ environment = "double_stimulus"
 a_sens = 0.
 a_ips_left = 0.
 a_ips_right= 0.
-a_con_left = 0.5
-a_con_right = 0.5
-a_motor = 0.
-scale = 2.
-stimulus_sensitivity = 2
-social_sensitivity = 0
-social_weight_decay_rate = 0.01
+a_con_left = 1
+a_con_right = 1
+a_motor = 0.01
+scale = 2
+stimulus_sensitivity = 3
+social_sensitivity = 0.5
+social_weight_decay_rate = 0.0
+
+agent_stimulus_scale = 0.012
+
 
 f_sens = 5.
 f_motor = 5.
-k = 5
+k = 2
 a_soc_sens_left = 0.
 a_soc_sens_right = 0.
-a_soc_motor_left = 0.8
-a_soc_motor_right = 0.8
+a_soc_motor_left = 1
+a_soc_motor_right = 1
 n_oscillators = 5
 flavour = 'social'
 n_agents = 10
@@ -114,7 +117,6 @@ if flavour == "social":
         stimulus_scale, stimulus_sensitivity, movement_speed, agent_radius, agent_eye_angle, delta_orientation, stimulus_ratio, n_agents)
 
 elif flavour == "eco":
-    agent_stimulus_scale = 0.003
     agent_stimulus_decay_rate = 0.1
     env = Social_stimulus_environment(fs, duration, stimulus_positions, stimulus_decay_rate,
      stimulus_scale, stimulus_sensitivity, movement_speed, agent_radius, agent_eye_angle, delta_orientation, agent_stimulus_scale, agent_stimulus_decay_rate, stimulus_ratio, n_agents)
@@ -126,9 +128,101 @@ positions_x = run["x position"]
 positions_y = run["y position"]
 for a in range(n_agents):
     plt.plot(positions_x[a], positions_y[a])
+plt.title(run["approach score"])
+plt.show()
+
+def calculate_wPLI(phase_1, phase_2):
+   delta_phase = phase_1 - phase_2
+   Im = np.imag(np.exp(1j*(delta_phase)))
+   numer = np.abs(np.mean(np.abs(Im) * np.sign(Im)))
+   denom = np.mean(np.abs(Im))
+
+   if denom == 0:
+      denom = 1
+   return numer / denom
+
+
+def calculate_average_inter_agent_PLV(phase_matrices, window_length, window_step):
+    # calculate windowed PLV
+    window_start = 0
+    window_end = window_start + window_length
+    simulation_length =int(np.size(phase_matrices[0], 1))
+    plv_in_time = []
+    plv_5_in_time = []
+    interval_times = []
+    _n_oscillators = np.size(phase_matrices[0], 0)
+    n_agents = len(phase_matrices)
+    agent_combinations = n_agents * (n_agents + 1) / 2
+
+
+    # for the whole duration of the trial
+    while (window_start + window_length) < simulation_length:
+        interval_times.append(window_start + window_length/2)
+        plv = 0
+        counter = 0
+        plv_5 = 0
+
+            # loop through all the agent pairs
+        for a_i in range(n_agents):
+            for a_j in range(a_i, len(phase_matrices)):
+                
+                # for all the oscillators
+                for i in range(_n_oscillators):
+                    #plv += np.abs(np.mean(np.exp(1j *(phase_matrices[a_i][i, window_start:window_end] - phase_matrices[a_j][i, window_start:window_end]))))
+                    wpli = calculate_wPLI(phase_matrices[a_i][i, window_start:window_end] , phase_matrices[a_j][i, window_start:window_end])
+                    plv +=  wpli #np.abs(np.mean(np.exp(1j *(phase_matrices[a_i][i, window_start:window_end] - phase_matrices[a_j][i, window_start:window_end]))))
+                    if i == 4: # if you have a fifth oscillator
+                        plv_5 += np.abs(np.mean(np.exp(1j *(phase_matrices[a_i][i, window_start:window_end] - phase_matrices[a_j][i, window_start:window_end]))))
+                    counter += 1 
+        window_start += window_step
+        window_end += window_step
+                  
+
+        plv_in_time.append(plv / counter)
+        plv_5_in_time.append(plv_5 / (counter/_n_oscillators) )
+    mean_plv = np.mean(plv_in_time)
+    mean_plv_5 = np.mean(plv_5_in_time)
+
+
+    return plv_in_time, interval_times, mean_plv, plv_5_in_time, mean_plv_5
+
+
+def calculate_average_PLV(phase_matrix, window_length, window_step):
+   # calculate windowed PLV
+   window_start = 0
+   window_end = window_start + window_length
+   simulation_length =int(np.size(phase_matrix, 1))
+   plv_in_time = []
+   interval_times = []
+   _n_oscillators = np.size(phase_matrix, 0)
+   oscillator_combinations = _n_oscillators * (_n_oscillators - 1) / 2
+
+
+   while (window_start + window_length) < simulation_length:
+      interval_times.append(window_start + window_length/2)
+      plv = 0
+      counter = 0
+      for i in range(_n_oscillators):
+         for j in range(i+1, _n_oscillators): # i+1 because dont want connection of oscillator with itself
+            plv += np.abs(np.mean(np.exp(1j *(phase_matrix[i, window_start:window_end] - phase_matrix[j, window_start:window_end]))))
+            window_start += window_step
+            window_end += window_step
+            counter += 1
+      plv_in_time.append(plv / oscillator_combinations)
+   mean_plv = np.mean(plv_in_time)
+
+   return plv_in_time, interval_times, mean_plv
+
+phase_matrices = run["phases"]
+window_length = fs
+window_step = fs/10
+plv_in_time, interval_times, mean_plv, plv_5_in_time, mean_plv_5 = calculate_average_inter_agent_PLV(phase_matrices, window_length, window_step)
+
+plt.plot(interval_times, plv_in_time)
 plt.show()
 
 plot_multi_agent_run(stimulus_ratio, stimulus_decay_rate, stimulus_scale, positions_x, positions_y , n_agents)
+multi_agent_animation(stimulus_ratio, stimulus_decay_rate, stimulus_scale, positions_x, positions_y, n_agents, duration, fs, True)
 
 
 # choose one of the players
@@ -147,88 +241,4 @@ print(KOP_std)
 plt.plot(interval_times, plv_in_time)
 plt.plot(KOP_in_time)
 plt.show()
-
-
-
-# open the grid search results 
-with open(r"results/GridSearchResults_single_stimulus_4_1810.pickle", "rb") as input_file:
-   grid_results = pickle.load(input_file)
-
-grid_results = average_grid_serach(grid_results)
-
-#show_grid_search_results(grid_results, 10)
-
-max_mean_agent, min_mean_agent, max_min_agent = find_agents(grid_results)
-max_mean_agent = max_mean_agent.to_dict()
-print(max_mean_agent)
-
-a_sens = max_mean_agent["a_sens"]
-a_ips_left = max_mean_agent["a_ips_left"]
-a_ips_right= max_mean_agent["a_ips_right"]
-a_con_left = max_mean_agent["a_con_left"]
-a_con_right = max_mean_agent["a_con_right"]
-a_motor = max_mean_agent["a_motor"]
-scale = max_mean_agent["scaling_factor"]
-stimulus_sensitivity = max_mean_agent["sensitivity"]
-f_sens = max_mean_agent["f_sens"]
-f_motor = max_mean_agent["f_motor"]
-k = max_mean_agent["k"]
-
-
-
-
-
-
-
-
-def visualize_grid_search(grid_results, x_axis, y_axis, other_parameters):
-   """
-   plots the results of the grid search on two specified dimension
-
-   Arguments:
-   -----------
-   grid_results: pandas dataframe
-
-   x_axis: string
-
-   y_axis: string
-
-   other_parameters: dictionary
-      the values of the parameters that stay fixed
-
-   
-   """
-   # extract requested values
-   for key in other_parameters:
-      if not ( (key == x_axis) or (key == y_axis) ):
-
-         # make subselection of fixed parameters
-         grid_results = grid_results[grid_results[key] == other_parameters[key]]
-
-   # for the other parameters, make a numpy array to plot
-   x_axis_values = np.sort(np.unique(grid_results[x_axis].to_numpy()))
-   y_axis_values = np.sort(np.unique(grid_results[y_axis].to_numpy()))
-   plotting_array = np.zeros((len(x_axis_values), len(y_axis_values)))
-
-   
-   for x in range(len(x_axis_values)):
-      for y in range(len(y_axis_values)):
-         plot_val = grid_results[grid_results[x_axis] == x_axis_values[x]]
-         print(plot_val)
-         plot_val = plot_val[grid_results[y_axis] == y_axis_values[y]]
-         print(plot_val)
-         plotting_array[x, y] = float(plot_val.performance.to_numpy())
-
-
-   plt.xticks(np.arange(0, len(x_axis_values), 1), x_axis_values)
-   plt.yticks(np.arange(0, len(y_axis_values), 1), y_axis_values)
-   plt.xlabel(x_axis)
-   plt.ylabel(y_axis)
-   plt.imshow(plotting_array)
-   plt.colorbar()
-   plt.show()
-
-
-
-
 
